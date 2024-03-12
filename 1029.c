bdb15bbca114579fa42925eef1acec789b4caf6e
snowykte0426 <snowykte0426@naver.com>
2024-03-12 17:20:43 +0900

    1028~1030 파일 업로드

---
+++ 1029.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*
+#include <stdio.h>
+int main() {
+double X1 = 0.0;
+scanf("%lf", &X1);
+printf("%.11lf", X1);
+return 0;
+*/
---
+++ 1030.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 
+/*
+#include <stdio.h>
+int main() {
+long long dnum1 = 0;
+scanf("%lld", &dnum1);
+printf("%lld", dnum1);
+return 0;
+}
+*/
